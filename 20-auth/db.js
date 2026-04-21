fd61b21d20cf9a517c2962c4ca0f5bca3aeae16f
Alpkavakli <alp.kavakli@gmail.com>
2026-04-21 15:39:22 +0300

    a

---
+++ 20-auth/db.js
@@ -0,0 +1,12 @@
+import mysql from 'mysql2/promise';
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'std',
+  password: '',
+  database: 'test',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
+export default db;
